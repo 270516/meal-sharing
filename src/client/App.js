6f32236fd1248c41572f7326ff06b81beb02117d
270516 <ranjankomal93@gmail.com>
2024-03-09 11:51:07 +0100

    Added the meail/:id and implemented little css

--- src/client/App.js
+++ src/client/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import TestComponent from "./components/TestComponent/TestComponent";
 import MealsList from "./components/TestComponent/MealsList";
 import HomePage from "./components/TestComponent/Homepage";
+import MealDetails from "./components/TestComponent/MealDetails";
 
 function App() {
   return (
@@ -13,15 +14,17 @@ function App() {
           <Route exact path="/">
             <HomePage />
           </Route>
-          <Route exact path="/potato">
+          <Route exact path="/lol">
             <p>lol</p>
           </Route>
           <Route exact path="/test-component">
             <TestComponent></TestComponent>
           </Route>
-          {/* Route to render MealsList component */}
           <Route exact path="/meals">
             <MealsList />
+          </Route>
+          <Route exact path="/meals/:id">
+            <MealDetails />
           </Route>
         </Switch>
       </div>
